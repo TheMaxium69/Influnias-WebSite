3af34b8a9bcbbf1897ec57caa8d9c4b2ae20ad77
Maxime Tournier <mxm.tournier@gmail.com>
2022-10-17 22:29:57 +0200

    test

--- app/extension.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "env.php";
-
-require_once $env_urlFile . "Boostrap.phtml";
-require_once $env_urlFile . "FontAwesome.phtml";
-require_once $env_urlFile . "Google.phtml";
-require_once $env_urlFile . "Normalize.phtml";
-require_once $env_urlFile . "TyroLink.phtml";
