8c11d8696e9f1927af46e88390a45dbecb5f5ee0
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-10-19 03:18:46 +0200

    bug

--- api.php
+++ api.php
@@ -7,19 +7,19 @@ if(isset($_POST['influnias-name']) && isset($_POST['influnias-email'])&& isset($
 
     if( !empty($_POST['influnias-name']) && !empty($_POST['influnias-email']) &&  !empty($_POST['influnias-text']) ){
 
-        $name = $_POST['influnias-name'];
-        $mail = $_POST['influnias-email'];
-        $content = $_POST['influnias-text'];
+        $name = "*" . $_POST['influnias-name'] . "*";
+        $mail = "*" . $_POST['influnias-email'] . "*";
+        $content = "*" . $_POST['influnias-text'] . "*";
 
                 $requeteInsert = "INSERT INTO influnias(name, mail, content) VALUES ('$name', '$mail', '$content')";
 
                 $resultInsert = mysqli_query($ConnectDB, $requeteInsert);
 
-                header("location: https://influnias.fr/#contact?true=1");
+                header("location: https://www.influnias.fr/#contact?true=1");
 
     }else{
         
-        header("location: https://influnias.fr/#contact?false=0");
+        header("location: https://www.influnias.fr/#contact?false=0");
 
     }
 
