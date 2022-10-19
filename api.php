e224dd963a574d468bc4b2e9e71f137017a450cc
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2022-10-19 02:10:11 +0200

    final

---
+++ api.php
@@ -0,0 +1,33 @@
+<?php
+
+
+include "/home/tyroliumfg/DB-tyroliumfguser.php";
+
+if(isset($_POST['influnias-name']) && isset($_POST['influnias-email'])&& isset($_POST['influnias-text'])){
+
+    if( !empty($_POST['influnias-name']) && !empty($_POST['influnias-email']) &&  !empty($_POST['influnias-text']) ){
+
+        $name = $_POST['influnias-name'];
+        $mail = $_POST['influnias-email'];
+        $content = $_POST['influnias-text'];
+
+                $requeteInsert = "INSERT INTO influnias(name, mail, content) VALUES ('$name', '$mail', '$content')";
+
+                $resultInsert = mysqli_query($ConnectDB, $requeteInsert);
+
+                header("location: https://influnias.fr/#contact?true=1");
+
+    }else{
+        
+        header("location: https://influnias.fr/#contact?false=0");
+
+    }
+
+}
+
+
+
+
+
+
+?>
